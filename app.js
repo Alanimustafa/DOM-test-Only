281f6f08b096f77b91fbc305fc73648565774aa5
Alanimustafa <mustafa.kamel.ani@gmail.com>
2024-12-05 13:50:03 -0600

    DOM - querrySelector - Test Resume

---
+++ app.js
@@ -0,0 +1,22 @@
+function education () {
+  const educationButton = document.querySelector('.theScreen');
+  educationButton.innerHTML = "B. Sc. Software Engineering 07/2000."
+}
+
+function workExperience () {
+  const work = document.querySelector('.theScreen');
+  work.innerHTML = "Audio/Visual Designer and Consultant"
+}
+
+function skills () {
+  const skills = document.querySelector('.theScreen');
+  skills.innerHTML = `JavaScript Programming \n 
+  React Development \n 
+  Node.js \n 
+  Express.js`;
+}
+
+function contacts () {
+  const contact = document.querySelector('.theScreen');
+  contact.innerHTML = "https://www.linkedin.com/in/mustafa-m-alani/"
+}
